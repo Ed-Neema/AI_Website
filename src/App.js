aac5eb4a68b4517c003dfd37e457a134a0a8dd7f
Ed-Neema <ednahakoth21@gmail.com>
2022-11-16 00:28:32 +0200

    Project files added

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./App.css"; //main styles
+import {Brand, CTA, Navbar} from './components';
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers'
+const App = () => {
+  return (
+    <div className='App'>
+
+    <div className="gradient__bg">
+      <Navbar/>
+      <Header/>
+    </div>
+
+    <Brand/>
+    <WhatGPT3/>
+    <Features/>
+    <Possibility/>
+    <CTA/>
+    <Blog/>
+    <Footer/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/brand/imports.js
@@ -0,0 +1,8 @@
+// Helper file to import all images into our brand component
+import google from "../../assets/google.png";
+import slack from "../../assets/slack.png";
+import atlassian from "../../assets/atlassian.png";
+import dropbox from "../../assets/dropbox.png";
+import shopify from "../../assets/shopify.png";
+
+export { google, slack, atlassian, dropbox, shopify };
